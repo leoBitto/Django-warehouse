8d22d8ca4a2a58c5158d0ebb3dd5c03fe83fde67
leoBitto <leonardobitto@gmail.com>
2025-03-01 19:03:29 +0100

    added urls

--- urls.py
+++ urls.py
@@ -1,27 +1,14 @@
-# inventory/urls.py
-
 from django.urls import path
-from .views.base import *
-
+from . import views
 
 app_name = 'warehouse'
 
 urlpatterns = [
-    path('categories/', ProductCategoryView.as_view(), name='product_category_view'),
-    path('products/', ProductView.as_view(), name='product_view'),
-    path('products/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
-    path('download-stock-data/', DownloadStockDataCSV.as_view(), name='download_stock_data'),
-    path('sales/', SaleListView.as_view(), name='sale_list'),
-    path('sale/<int:sale_id>/', SaleDetailView.as_view(), name='sale_detail'),
-    path('orders/', OrderListView.as_view(), name='order_list'),
-    path('order/<int:order_id>/', OrderDetailView.as_view(), name='order_detail'),
-    path('invoice/<str:invoice_type>/<str:invoice_number>/', InvoiceDetailView.as_view(), name='invoice_detail'),
-
-    path('global_report/', GlobalReportView.as_view(), name='global_report'),
-    path('product_report/', ProductReportView.as_view(), name='product_report'),
-    path('data_quality_report/', DataQualityReportView.as_view(), name='data_quality_report'),
-    path('sales_report/', SalesReportView.as_view(), name='sales_report'),
-    path('orders_report/', OrdersReportView.as_view(), name='orders_report'),
-
+    # Product URLs
+    path('products/', views.ProductListView.as_view(), name='product_list'),
+    path('products/<int:product_id>/', views.ProductDetailView.as_view(), name='product_detail'),
     
+    # Product Category URLs
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:category_id>/', views.CategoryDetailView.as_view(), name='category_detail'),
 ]
